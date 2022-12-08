90a6e308dfc8cdc5cb1da3d2d32e329dcae1e14f
digitrom <roma.bobrovski@gmail.com>
2022-12-08 15:41:07 +0300

    create-81.js

---
+++ js/81.js
@@ -0,0 +1,9 @@
+const myDog = {
+    // Only change code below this line
+    "name": "Miki",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["Mili", "Sasha"]
+
+    // Only change code above this line
+};
