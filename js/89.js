d5c34ea99c6280e865843058e5e159cc6cfbfaa5
digitrom <roma.bobrovski@gmail.com>
2022-12-08 17:49:33 +0300

    create-89.js

---
+++ js/89.js
@@ -0,0 +1,20 @@
+const myMusic = [
+    {
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ],
+        "gold": true
+    },
+    {
+        "artist": "Futurecop!",
+        "title": "From Oceans Within",
+        "release_year": 2022,
+        "formats": ["Flac", "CD", "Mp3"]
+    }
+];
+
